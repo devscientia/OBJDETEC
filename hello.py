43c23b6fc0151ea702f98c2ebc067bd521f1ebf2
Robson Tavares Nonato <robson.tavares@scientia-e.com>
2026-03-25 17:29:17 +0000

    Teste de VM para desenvolvimento.

---
+++ hello.py
@@ -0,0 +1,3 @@
+print('Hello, World!')
+
+# This is a simple Python program that prints "Hello, World!" to the console. It serves as a basic example of how to write and run a Python script. To execute this code, save it in a file named "hello.py" and run it using a Python interpreter.
