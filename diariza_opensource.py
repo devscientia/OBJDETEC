bc691ada52e91e8223bbdc7a58326c164d5ea666
Robson Tavares Nonato <robson.tavares@scientia-e.com>
2026-03-27 17:48:40 +0000

    Novo arquivo de audio

--- diariza_opensource.py
+++ diariza_opensource.py
@@ -23,7 +23,7 @@ from pyannote.audio import Pipeline
 # You must generate a token from hf.co/settings/tokens and accept user conditions for pyannote/speaker-diarization-community-1
 pipeline = Pipeline.from_pretrained("pyannote/speaker-diarization-community-1", use_auth_token="YOUR_HUGGINGFACE_ACCESS_TOKEN")
 # Run the pipeline on an audio file
-diarization = pipeline("audio.wav")
+diarization = pipeline("atend_teste_01.wav")
 for turn, _, speaker in diarization.itertracks(yield_label=True):
     print(f"[{turn.start:.1f}s - {turn.end:.1f}s] {speaker}")
 
